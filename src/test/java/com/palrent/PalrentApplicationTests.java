6d4bb3ef7d5bc0a74a9bd43bbbb9ec5562528df0
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-07-09 15:15:16 +0300

    first commit

---
+++ src/test/java/com/palrent/PalrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
