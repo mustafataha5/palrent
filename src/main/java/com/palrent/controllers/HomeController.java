600dfc77c793f50d4bcaf5dc380cba0131b7a988
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-07-10 22:06:26 +0300

    apartment details page

--- src/main/java/com/palrent/controllers/HomeController.java
+++ src/main/java/com/palrent/controllers/HomeController.java
@@ -1,20 +1,21 @@
 package com.palrent.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.palrent.models.User;
 
-import jakarta.validation.Valid;
-
 @Controller
 public class HomeController {
 	@GetMapping("/")
-	public String homePage(@ModelAttribute("newUser")User user ) {
+	public String homePage(@ModelAttribute("newUser") User user) {
 		return "main/home.jsp";
-		}
-	
+	}
+
+	@GetMapping("/apartment")
+	public String getMethodName() {
+		return "apartment/apartmentdetails.jsp";
+	}
 
 }
