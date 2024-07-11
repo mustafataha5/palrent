63cf6af3d4c83fabed33561ae92daee5ca103dac
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-07-11 15:19:38 +0300

    adding offer to the edit page for the user

--- src/main/java/com/palrent/controllers/ApartmentController.java
+++ src/main/java/com/palrent/controllers/ApartmentController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.palrent.models.Department;
+import com.palrent.models.Offer;
 import com.palrent.models.User;
 import com.palrent.services.ApartmentService;
 import com.palrent.services.OfferService;
@@ -21,71 +23,67 @@ import com.palrent.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
-
 @Controller
 public class ApartmentController {
 	@Autowired
 	ApartmentService apartmentService;
 	@Autowired
-	OfferService offerService ; 
+	OfferService offerService;
 	@Autowired
-	UserService userService ; 
+	UserService userService;
 	@Autowired
-	RuleServices ruleServices ; 
-	
-	
-	
+	RuleServices ruleServices;
 
 	@GetMapping("/user/apartment/new")
-	public String getApartment(@ModelAttribute("Apartment") Department apartment,HttpSession session) {
-		if(session.getAttribute("userId")==null) {
+	public String getApartment(@ModelAttribute("Apartment") Department apartment, HttpSession session) {
+		if (session.getAttribute("userId") == null) {
 			return "redirect:/";
 		}
 		return "user/apartment/newpartment.jsp";
 	}
 
 	@PostMapping("/user/apartment/new")
-	public String addApartmentPosting(@Valid @ModelAttribute("Apartment") Department apartment,
-			BindingResult result
-			,HttpSession session) {
+	public String addApartmentPosting(@Valid @ModelAttribute("Apartment") Department apartment, BindingResult result,
+			HttpSession session) {
 		if (result.hasErrors()) {
 			return "user/apartment/newpartment.jsp";
 		}
 		User user = userService.findUser((Long) session.getAttribute("userId"));
 		apartment.setOwner(user);
-		System.out.println(">>>>>>>>>>"+user.getEmail());
+		System.out.println(">>>>>>>>>>" + user.getEmail());
 		apartmentService.creatAdminApartment(apartment);
 
 		return "redirect:/user/apartment";
 	}
-	
+
 	@GetMapping("/user/apartment")
-	public String showUserApartment(HttpSession session,Model model){
-		if(session.getAttribute("userId")==null) {
+	public String showUserApartment(HttpSession session, Model model) {
+		if (session.getAttribute("userId") == null) {
 			return "redirect:/";
 		}
-		model.addAttribute("user", userService.findUser((Long)session.getAttribute("userId") ));
+		model.addAttribute("user", userService.findUser((Long) session.getAttribute("userId")));
 		return "user/apartment/apartment.jsp";
 	}
-	
+
 	@GetMapping("/user/apartment/{id}/edit")
-	public String userApartmentPutMapping(@PathVariable("id") Long id, Model model ,HttpSession session) {
-		if(session.getAttribute("userId")==null) {
+	public String userApartmentPutMapping(@PathVariable("id") Long id, Model model, HttpSession session) {
+		if (session.getAttribute("userId") == null) {
 			return "redirect:/";
 		}
 		Department apartment = apartmentService.findById(id);
 		model.addAttribute("Apartment", apartment);
-		model.addAttribute("exOffer",offerService.allOffernotIn(id));
-		model.addAttribute("exRule",ruleServices.allRulenotIn(id));
+		model.addAttribute("exOffer", offerService.allOffernotIn(id));
+		model.addAttribute("exRule", ruleServices.allRulenotIn(id));
 
 		return "/user/apartment/editapartment.jsp";
 
 	}
+
 	@PatchMapping("/user/apartment/{id}/edit")
 	public String userApartmentPatchPosting(@Valid @ModelAttribute("Apartment") Department apartment,
 			BindingResult result, @PathVariable("id") Long id, Model model) {
 		if (result.hasErrors()) {
-			model.addAttribute("exOffer",offerService.allOffernotIn(id));
+			model.addAttribute("exOffer", offerService.allOffernotIn(id));
 			return "user/apartment/editapartment.jsp";
 		}
 		Department EditedApartment = apartmentService.findById(id);
@@ -106,12 +104,35 @@ public class ApartmentController {
 		return "redirect:/user/apartment";
 
 	}
-	
+
 	@DeleteMapping("/user/apartment/{id}/delete")
-	public String deleteUserApartment(@PathVariable("id")Long id) {
+	public String deleteUserApartment(@PathVariable("id") Long id) {
 		apartmentService.deleteApartment(id);
-		return"redirect:/user/apartment";
+		return "redirect:/user/apartment";
 	}
-	
-	
+
+	@PatchMapping("/user/apartment/{id}/AddOffer")
+	public String addOffer(@PathVariable("id") Long Id, @RequestParam(value = "offerId", required = false) Long offerId,
+			Model model) {
+		if (offerId == null) {
+			return "redirect:/user/apartment/" + Id + "/edit";
+		}
+		Department department = apartmentService.findById(Id);
+		Offer offer = offerService.findOffer(offerId);
+		department.getOffers().add(offer);
+		apartmentService.updateApartment(department);
+
+		return "redirect:/user/apartment/" + Id + "/edit";
+	}
+
+	@DeleteMapping("/user/apartmet/{id}/DelOffer")
+	public String delOffer(@PathVariable("id") Long Id, @RequestParam("offerId") Long offerId, Model model) {
+		Department department = apartmentService.findById(Id);
+		Offer offer = offerService.findOffer(offerId);
+		department.getOffers().remove(offer);
+		apartmentService.updateApartment(department);
+
+		return "redirect:/user/apartment/" + Id + "/edit";
+	}
+
 }
